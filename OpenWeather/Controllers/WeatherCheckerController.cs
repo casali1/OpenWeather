32fadc0ad9a55aa8d403b1ac75e4407e7f6b90ea
casali1 <frank_casali@hotmail.com>
2019-05-30 15:12:44 -0600

    Consuming OpenWeather API.

---
+++ OpenWeather/Controllers/WeatherCheckerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenWeather.Models;
+
+namespace OpenWeather.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherCheckerController : Controller
+    {
+        [HttpGet]
+        public ActionResult City(string city)
+        {
+            return View(new Data { Temp = "12", Summary = "Barmy", City = "NY" });
+        }
+    }
+}
---
+++ OpenWeather/Models/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenWeather.Models
+{
+    //return View(new { Temp = "12", Summary = "Barmy", City = "NY" });
+
+    public class Data
+    {
+        public string Temp { get; set; }
+        public string Summary { get; set; }
+        public string City { get; set; }
+    }
+}
